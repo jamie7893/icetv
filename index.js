00e88c647cd546cab25bc4b337107568b6b29ffa
jamie <jamie7893@live.com>
2016-04-15 23:30:03 -0500

    use sockets and make leave chat room

--- index.js
+++ index.js
@@ -3,69 +3,73 @@
 let server;
 
 const
-  express = require('express'),
-  app = express(),
-  path = require('path'),
-  bodyParser = require('body-parser'),
-  morgan = require('morgan'),
-  multer = require('multer'),
-  Sequelize = require('sequelize'),
-  bcrypt = require('bcrypt-nodejs'),
-  crypto = require('crypto'),
-  uuid = require('uuid'),
-  config = require('./config.json'),
-  cookieParser = require('cookie-parser'),
-  sessionFileStore = require('session-file-store'),
-  session = require('express-session');
+    express = require('express'),
+    app = express(),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    multer = require('multer'),
+    Sequelize = require('sequelize'),
+    bcrypt = require('bcrypt-nodejs'),
+    crypto = require('crypto'),
+    uuid = require('uuid'),
+    config = require('./config.json'),
+    cookieParser = require('cookie-parser'),
+    sessionFileStore = require('session-file-store'),
+    _ = require('lodash'),
+    session = require('express-session');
+
 
 let
-  FileStore = sessionFileStore(session);
+    FileStore = sessionFileStore(session);
 
 
 // Cookies
 app.set('trust proxy', 1); // trust first proxy
 var sess = {
-  genId: function(req) {
-    return uuid.v4();
-  },
-  name: 'thesis-sessions',
-  secret: uuid.v4(),
-  saveUnitialized: true,
-  resave: true,
-  store: new FileStore(),
-  cookie: {
-    secure: false
-  }
+    genId: function(req) {
+        return uuid.v4();
+    },
+    name: 'thesis-sessions',
+    secret: uuid.v4(),
+    saveUnitialized: true,
+    resave: true,
+    store: new FileStore(),
+    cookie: {
+        secure: false
+    }
 };
 
 if (app.get('env') === 'production') {
-  app.set('trust proxy', 1); // trust first proxy
-  sess.cookie.secure = true; // serve secure cookies
+    app.set('trust proxy', 1); // trust first proxy
+    sess.cookie.secure = true; // serve secure cookies
 }
 
 app
-  .use(morgan('dev')) // logs request to the console
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(session(sess))
-  .use(cookieParser())
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({
-    extended: true
-  }));
+    .use(morgan('dev')) // logs request to the console
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(session(sess))
+    .use(cookieParser())
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({
+        extended: true
+    }));
 
 // Uploading Images
 var uploading = multer({
-  dest: './public/images/profile/',
-  limits: {
-    fileSize: 1000000,
-    files: 1
-  }
+    dest: './public/images/profile/',
+    limits: {
+        fileSize: 1000000,
+        files: 1
+    }
 });
 
 
 module.exports.close = function() {
-  console.log('shutting down the server...');
-  server.close();
+    console.log('shutting down the server...');
+    server.close();
 };
 // sequelize initialization //
 // for heroku
@@ -77,41 +81,127 @@ const sequelize = new Sequelize('postgres://postgres:admin@localhost:3000/postgr
 // example
 // const colorsService = require("./service/colors")(sequelize);
 const
-  userService = require("./service/user.js")(sequelize),
-  chatService = require("./service/chat.js")(sequelize);
+    userService = require("./service/user.js")(sequelize),
+    chatService = require("./service/chat.js")(sequelize);
 
 var
-  Chat = sequelize.import('./model/chatroom.js'),
-  User = sequelize.import('./model/user.js'),
-  UserChat = sequelize.import('./model/userchatroomjct.js'),
-  Creds = sequelize.import('./model/credentials.js');
+    Chat = sequelize.import('./model/chatroom.js'),
+    User = sequelize.import('./model/user.js'),
+    UserChat = sequelize.import('./model/userchatroomjct.js'),
+    Creds = sequelize.import('./model/credentials.js');
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+
+    });
+
+    socket.on('joinedChat', function(data) {
+        var chatId = data.chatId;
+        var chatroomSocket = setInterval(function() {
+            var messages = [];
+            var users = [];
+            Chat.findAll({
+                where: {
+                    idChatroom: chatId
+                }
+            }).then(function(messages) {
+                var gettingMSG = _.map(messages, function(message) {
+                    return User.findOne({
+                        where: {
+                            id: message.idSender
+                        },
+                    }).then(function(foundUser) {
+                        message.dataValues.username = foundUser.dataValues.username;
+                        message.dataValues.img = foundUser.dataValues.img;
+                        return message.dataValues;
+                    });
+                });
+
+
+                UserChat.findAll({
+                  where: {
+                    idChatroom: chatId
+                  }
+                }).then(function(chatroomUsers) {
+                    var gettingUsers = _.map(chatroomUsers, function(chatroomUser) {
+                        return User.findOne({
+                            where: {
+                                id: chatroomUser.idUser
+                            },
+                            attributes: ['username', 'img']
+                        }).then(function(chatUsername) {
+                            chatroomUser.dataValues.username = chatUsername.dataValues.username;
+                            chatroomUser.dataValues.img = chatUsername.dataValues.img;
+                            return chatroomUser.dataValues;
+                        });
+                    }); // end map
+                    Promise.all(gettingMSG).then(function(message) {
+                        Promise.all(gettingUsers).then(function(user) {
+                            socket.emit('message', {
+                                messages: message,
+                                users: user
+                            });
+                        });
+                    });
+                });
+            });
+        }, 1000);
+        socket.on('leaveChat', function(data) {
+            clearInterval(chatroomSocket);
+            UserChat.destroy({
+                where: {
+                    idUser: data.idUser
+                }
+            });
+        });
+      });
+    //
+    // setInterval(function() {
+    //     socket.emit('message', {
+    //         message: i
+    //     });
+    //     i++;
+    // }, 1000);
+
+
+
+});
+
+
 
 sequelize.sync().then(function(res) {
-    Chat.sync();
-    User.sync();
-    UserChat.sync();
-    Creds.sync();
+        Chat.sync();
+        User.sync();
+        UserChat.sync();
+        Creds.sync();
 
-    app.route('/logout')
-      .get(userService.logout);
-    app.route('/updateprofile')
-      .post(uploading.single('file'), userService.updateprofile);
-    app.route('/signup')
-      .post(userService.create);
-    app.route('/login')
-      .post(userService.login);
-    app.route('/joinchat')
-      .post(chatService.join)
-      .get(chatService.get);
-    app.route('/createMSG')
-      .post(chatService.createMSG)
-      .get(chatService.getMSG);
+        app.route('/logout')
+            .get(userService.logout);
+        app.route('/updateprofile')
+            .post(uploading.single('file'), userService.updateprofile);
+        app.route('/signup')
+            .post(userService.create);
+        app.route('/login')
+            .post(userService.login);
+        app.route('/joinchat')
+            .post(chatService.join)
+            .get(chatService.get);
+        app.route('/createMSG')
+            .post(chatService.createMSG)
+            .get(chatService.getMSG);
 
-    server = app.listen(process.env.PORT || 1738, process.env.IP || "0.0.0.0", function() {
-      var addr = server.address();
-      console.log("Server listening at", addr.address + ":" + addr.port);
+        // server = app.listen(process.env.PORT || 1738, process.env.IP || "0.0.0.0", function() {
+        //     var addr = server.address();
+        //     console.log("Server listening at", addr.address + ":" + addr.port);
+        // });
+        server = http.listen(1738, function() {
+            var addr = server.address();
+            console.log("Server listening at", addr.address + ":" + addr.port);
+
+        });
+    })
+    .catch(function(e) {
+        console.log('Error in sequelize.sync(): ' + e);
     });
-  })
-  .catch(function(e) {
-    console.log('Error in sequelize.sync(): ' + e);
-  });
--- public/js/chatroom/controller.js
+++ public/js/chatroom/controller.js
@@ -1,7 +1,7 @@
 angular.module('thesis.chatroom', ['luegg.directives', 'emoji', 'vkEmojiPicker', 'mgcrea.ngStrap'])
 
-.controller('ChatroomController', ['$scope', '$location', '$window', '$cookies', '$rootScope', '$http', 'UserService',
-    function AdminUserCtrl($scope, $location, $window, $cookies, $rootScope, $http, UserService) {
+.controller('ChatroomController', ['$scope', '$location', '$window', '$cookies', '$rootScope', '$http', 'UserService', 'chatSocket',
+    function AdminUserCtrl($scope, $location, $window, $cookies, $rootScope, $http, UserService, chatSocket) {
         if (!$cookies.get('id')) {
             $location.path("/login");
         } else {
@@ -9,36 +9,52 @@ angular.module('thesis.chatroom', ['luegg.directives', 'emoji', 'vkEmojiPicker',
             $scope.messages = [];
             $scope.id = $rootScope.id;
             var chatId = $scope.id;
+            chatSocket.emit('joinedChat', {
+                chatId: $scope.id
+            });
 
-  
-            var refreshUsers = function() {
-                $http({
-                    method: 'GET',
-                    url: '/joinchat'
-                }).then(function successCallback(response) {
-                    $scope.users = $.map(response.data, function(chat) {
-                        if (chat.idChatroom === chatId) {
-                            return chat;
-                        }
-                    });
-                    setTimeout(refreshUsers, 1000);
-                });
-            };
-            var refreshMsgs = function() {
-                $http({
-                    method: 'GET',
-                    url: '/createMSG'
-                }).then(function successCallback(response) {
-                    $scope.messages = $.map(response.data, function(data) {
-                        if (data.idChatroom === chatId) {
-                            return data;
-                        }
-                    });
-                    setTimeout(refreshMsgs, 1000);
-                });
-            };
-            refreshUsers();
-            refreshMsgs();
+            chatSocket.on('message', function(data) {
+                $scope.messages = data.messages;
+                console.log(data);
+                $scope.users = data.users;
+            });
+
+
+
+            $scope.$on('$destroy', function() {
+              chatSocket.emit('leaveChat', {
+                  idUser: $cookies.get('id')
+              });
+            });
+
+            // var refreshUsers = function() {
+            //     $http({
+            //         method: 'GET',
+            //         url: '/joinchat'
+            //     }).then(function successCallback(response) {
+            //         $scope.users = $.map(response.data, function(chat) {
+            //             if (chat.idChatroom === chatId) {
+            //                 return chat;
+            //             }
+            //         });
+            //         setTimeout(refreshUsers, 1000);
+            //     });
+            // };
+            // var refreshMsgs = function() {
+            //     $http({
+            //         method: 'GET',
+            //         url: '/createMSG'
+            //     }).then(function successCallback(response) {
+            //         $scope.messages = $.map(response.data, function(data) {
+            //             if (data.idChatroom === chatId) {
+            //                 return data;
+            //             }
+            //         });
+            //         setTimeout(refreshMsgs, 1000);
+            //     });
+            // };
+            // refreshUsers();
+            // refreshMsgs();
             $scope.createMSG = function(msg) {
                 if ($cookies.get('id')) {
                     UserService.createMSG(msg, chatId, $cookies.get('id')).then(function(data) {});
