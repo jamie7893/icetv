16108d57cc86c4b1f20edc0893839b947d3dc95e
jamie <jamie7893@live.com>
2017-05-18 20:55:52 -0500

    fix emote sub and streamer icons

--- src/js/components/Chat/Message.js
+++ src/js/components/Chat/Message.js
@@ -20,8 +20,9 @@ var Message = React.createClass({
               <div class="media">
 
                 <span class="chat-username">
-
-                 {this.props.message.user.isChatSponsor ? <img src="https://yt3.ggpht.com/egUeEEJTpKOlv3Rl3hTPdO52cK46oPhWKkl4G503EAhAswwDiwrt0UeFebUvaFYQttyvTSo2CQ=s16-nd" class="style-scope yt-live-chat-author-badge-renderer"></img> : null}
+                  {this.props.message.user.isChatOwner ? <img src="/images/streamer-icon.png" class="streamer style-scope yt-live-chat-author-badge-renderer"></img> : null}
+                 {this.props.message.user.isChatModerator ? <img src="/images/Moderator-icon.png" class="mod style-scope yt-live-chat-author-badge-renderer"></img> : null}
+                 {this.props.message.user.isChatSponsor ? <img src="https://yt3.ggpht.com/egUeEEJTpKOlv3Rl3hTPdO52cK46oPhWKkl4G503EAhAswwDiwrt0UeFebUvaFYQttyvTSo2CQ=s16-nd" class="sub style-scope yt-live-chat-author-badge-renderer"></img> : null}
                  {this.props.message.user.displayName}
 
               </span>
